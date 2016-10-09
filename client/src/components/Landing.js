fecd46fc69214d1fc79c673748ffb4c438fc950d
Mikhail Lindblad <lindbladmisha@hotmail.com>
2016-10-09 19:29:28 +0200

    Disables add button when empty location

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,14 +6,26 @@ import AppStore from '../stores/AppStore';
 
 const Landing = React.createClass({
   displayName: "Landing",
+  _onAdd() {
+    console.log(this.refs.locationInput.value);
+  },
+  _onChange(evt) {
+    this.setState({
+      hasAddDisabled: !evt.target.value || evt.target.value == ""
+    });
+  },
+  getInitialState() {
+    return {hasAddDisabled: true};
+  },
   render() {
+    let {hasAddDisabled} = this.state;
     return (
       <div className="ui white vertical segment">
         <div className="ui container main">
           <div className="ui raised segment">
             <div className="ui action input">
-              <input type="text" placeholder="Enter a location..."/>
-                <button className="ui blue icon button">
+              <input type="text" placeholder="Enter a location..." ref="locationInput" onChange={this._onChange}/>
+                <button className={"ui blue icon button"  + (hasAddDisabled ? " disabled" : "")} onClick={this._onAdd}>
                   <i className="plus icon" />
                 </button>
             </div>
